38b8f84746a501a30ee8dd48eec5bf46283b8dcc
Jesko Dujmovic <jeskod+git@gmail.com>
2019-03-25 23:01:05 +0100

    multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 extern crate num_bigint;
 extern crate num_traits;
 
-use num_bigint::ToBigUint;
+use num_bigint::{BigUint, ToBigUint};
 use num_traits::checked_pow;
 use std::collections::HashMap;
+use std::thread;
 
 const BASE: u32 = 10;
+const THREADS: i32 = 8;
 
 fn main() {
+    let mut children = vec![];
+
+    let start_at = checked_pow(10.to_biguint().unwrap(), 5).unwrap();
+
+    for i in 0..(THREADS as u32) {
+        // Spin up another thread
+        let current = &start_at + i;
+        children.push(thread::spawn(move || {
+            check_infinitly(current, THREADS);
+        }));
+    }
+
+    for child in children {
+        // Keeping main thread alive
+        let _ = child.join();
+    }
+}
+
+fn check_infinitly(start_at: BigUint, different: i32) {
     let mut m = HashMap::new();
-    let mut current = checked_pow(10.to_biguint().unwrap(), 5).unwrap();
+    let mut current = start_at;
     let mut max = 0;
     loop {
         let res = calc_persistence(current.to_radix_be(BASE), &mut m);
@@ -17,7 +38,7 @@ fn main() {
             max = res;
             println!("persistence: {}, number: {}", max, current);
         }
-        current = current + 1 as usize;
+        current = current + different as usize;
     }
 }
 
